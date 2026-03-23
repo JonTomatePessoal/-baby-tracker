d3176500ff11f7f6cf5fa2539c71b2f454e12f9c
Jonatas Rafael Tomate <jonatasrafaeltomate@Jonatass-MacBook-Air.local>
2026-03-23 12:03:52 -0300

    configurando firebase e corrigindo caminho do css

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI",
+  apiKey: "AIzaSyAzJKLLW8UWCYzTKzqP3tyw9N9zj4FdrAw",
+  authDomain: "baby-tracker-eabec.firebaseapp.com",
+  projectId: "baby-tracker-eabec",
+  storageBucket: "baby-tracker-eabec.firebasestorage.app",
+  messagingSenderId: "439787773713",
+  appId: "1:439787773713:web:fd4ef8d9a0a652aae6f6b0",
 };
 
 const app = initializeApp(firebaseConfig);
